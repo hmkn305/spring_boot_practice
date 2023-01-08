147e39df0d58d5a52ae280ccc80ff7eaa6128da2
hmkn305 <hmkn.305@gmail.com>
2023-01-08 23:10:14 +0900

    h2データベースの導入 mybatisの導入 テーブルの作成・データの追加 @RequiredArgsConstructorでDIの追加

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,18 +1,18 @@
 package com.example.its.web.issue;
 
 
-import com.example.its.domain.issue.IssueEntity;
 import com.example.its.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class IssueController {
 
-    private final IssueService issueService = new IssueService();
+    private final IssueService issueService;
 
     //Get /Issues
     @GetMapping("/issues")
