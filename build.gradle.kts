d86f0db65ea0ef681c3c51f0bf989d927b605f74
hmkn305 <hmkn.305@gmail.com>
2023-01-08 22:32:07 +0900

    リファクタリング lombokの導入 serviceクラスの生成

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,25 @@ group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.projectlombok:lombok:1.18.22")
+	implementation("org.projectlombok:lombok:1.18.22")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
---
+++ src/main/java/com/example/its/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.its.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+//コントローラであることを認識してくれる
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+         return "index";
+    }
+}
